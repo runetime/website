fe9a6ce034dbb44f799a697344917a1e0de47d15
Runis <stntlr@gmail.com>
2014-10-08 22:16:05 -0700

    Chat migrations

---
+++ database/migrations/2014_10_06_182207_create_chat_channels_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChatChannelsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('chat_channels', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name');
+			$table->string('name_trim');
+			$table->integer('messages');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('chat_channels');
+	}
+
+}
---
+++ database/migrations/2014_10_06_182409_create_chat_messages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChatMessagesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('chat_messages', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('author_id');
+			$table->mediumtext('contents');
+			$table->mediumtext('contents_parsed');
+			$table->integer('status');
+			$table->integer('channel');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('chat_messages');
+	}
+
+}
