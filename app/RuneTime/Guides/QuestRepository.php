b57d1dfc4548d3f3a39b7055794529eb98dc7b7b
Runis <stntlr@gmail.com>
2014-10-10 17:19:16 -0700

    Removed unnecessary override function

--- app/RuneTime/Guides/QuestRepository.php
+++ app/RuneTime/Guides/QuestRepository.php
@@ -5,11 +5,6 @@ class QuestRepository extends EloquentRepository{
 	public function __construct(Quest $model){
 		$this->model=$model;
 	}
-	public function getById($id){
-		return $this->model->
-			where('id','=',$id)->
-			first();
-	}
 	public function getByName($name){
 		return $this->model->
 			where('name','=',$name)->
