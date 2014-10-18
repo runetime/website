c900052200c80714f5e799bf9d9511b9e60219bc
Runis <stntlr@gmail.com>
2014-10-18 00:39:58 -0700

    Reformatted thread view

--- resources/views/forum/thread/view.blade.php
+++ resources/views/forum/thread/view.blade.php
@@ -2,17 +2,17 @@
 @section('contents')
 			<div class='wrapper'>
 				<div class='thread'>
-				<div class='row'>
-				    <div class='col-xs-4 col-sm-3 col-md-2 col-lg-1'>
-				        {!!\Image::userPhoto($thread->author_id)!!}
-				    </div>
-				    <div class='col-xs-8 col-sm-9 col-md-10 col-lg-11'>
-				        <h1>
-				            {{$thread->title}}
-				        </h1>
-				        by {!!\Link::name($thread->author_id)!!}, {{\Time::shortReadable($thread->created_at)}}
-				    </div>
-				</div>
+                    <div class='row'>
+                        <div class='col-xs-4 col-sm-3 col-md-2 col-lg-1'>
+                            {!!\Image::userPhoto($thread->author_id)!!}
+                        </div>
+                        <div class='col-xs-8 col-sm-9 col-md-10 col-lg-11'>
+                            <h1>
+                                {{$thread->title}}
+                            </h1>
+                            by {!!\Link::name($thread->author_id)!!}, {{\Time::shortReadable($thread->created_at)}}
+                        </div>
+                    </div>
 @if(!empty(json_decode($thread->tags)))
 					<ul class='list-inline'>
 	@foreach(json_decode($thread->tags) as $tag)
