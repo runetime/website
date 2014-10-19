63d0a0bfcc9c94cc713cf8843b4a2f23cb04f17d
Runis <stntlr@gmail.com>
2014-10-19 00:15:02 -0700

    Removed old style Presenters

--- app/RuneTime/Calculators/CalculatorPresenter.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace App\RuneTime\Calculators;
-use McCool\LaravelAutoPresenter\BasePresenter;
-use App,Input,Str,Request;
-class CalculatorPresenter extends BasePresenter{
-}
--- app/Runis/Accounts/UserPresenter.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace App\Runis\Accounts;
-use HTML;
-class UserPresenter{
-}
