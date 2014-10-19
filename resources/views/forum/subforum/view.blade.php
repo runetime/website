6199f7a87581a9e698efa057b0ff6cf5d9717d5e
Runis <stntlr@gmail.com>
2014-10-19 00:17:38 -0700

    Split subforum views into include files

--- resources/views/forum/subforum/view.blade.php
+++ resources/views/forum/subforum/view.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.forum')
 @section('forum')
-						<h1>
+				<h1>
 					{{$subforum->name}}
 				</h1>
 				<p class='text-muted'>
@@ -24,62 +24,12 @@
 				</div>
 @if(!empty($subforumList))
 	@foreach($subforumList as $subforumItem)
-				<div class='card card-read row'>
-					<div class='col-xs-12 col-sm-6 col-md-8'>
-						<h3>
-							<a href='/forums/{{\String::slugEncode($subforumItem->id, $subforumItem->name)}}'>
-								{{$subforumItem->name}}
-							</a>
-						</h3>
-						{{$subforumItem->description}}
-					</div>
-					<div class='col-xs-12 col-sm-6 col-md-1'>
-						{{$subforumItem->threads}} threads
-						<br />
-						{{$subforumItem->posts}} posts
-					</div>
-					<div class='col-xs-12 col-sm-12 col-md-3'>
-		@if(!empty($subforumItem->last_post_info))
-						<a href='/forum/thread/{{\String::slugEncode($subforumItem->last_thread_info->id, $subforumItem->last_thread_info->title)}}' title='{{$subforumItem->last_thread_title}}'>
-							{{$subforumItem->last_thread_info->title}}
-						</a>
-						<br />
-						by {!!\Link::name($subforumItem->last_post_info->author_id)!!}
-						<br />
-						<a href='/forum/thread/{{\String::slugEncode($subforumItem->last_thread_info->id, $subforumItem->last_thread_info->title)}}/last-post' title='{{$subforumItem->last_thread_title}}'>
-							{!!\Time::shortReadable($subforumItem->last_post_info->created_at)!!}
-						</a>
-		@endif
-					</div>
-				</div>
+		@include('forum.subforum._subforum', ['subforumItem' => $subforumItem])
 	@endforeach
 @endif
-@if(!empty($threads))
-	@foreach($threads as $thread)
-				<div class='card card-read row'>
-					<div class='col-xs-12 col-sm-6 col-md-8'>
-						<a href='/forums/thread/{{\String::slugEncode($thread->id, $thread->title)}}' title='{{$thread->title}}'>
-							{{$thread->title}}
-						</a>
-						<br />
-						Started by {!!\Link::name($thread->author_id)!!}, {{\Time::shortReadable($thread->created_at)}}
-		@foreach(json_decode($thread->tags) as $tag)
-						<a href='/forums/tag/{{$tag}}' class='label label-rt' title='{{$tag}}'>{{$tag}}</a>
-		@endforeach
-					</div>
-					<div class='col-xs-12 col-sm-6 col-md-1'>
-						{{$thread->posts-1}} posts
-						<br />
-						{{$thread->views}} views
-					</div>
-					<div class='col-xs-12 col-sm-12 col-md-3'>
-		@if($thread->last_post > 0)
-						{!!\Link::name($thread->last_post_info->author_id)!!}
-						<br />
-						{{\Time::shortReadable($thread->last_post_info->created_at)}}
-		@endif
-					</div>
-				</div>
+@if(!empty($threadList))
+	@foreach($threadList as $thread)
+		@include('forum.subforum._thread', ['thread' => $thread])
 	@endforeach
 @endif
 @stop
