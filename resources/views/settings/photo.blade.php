84e2dae9fc9853b9a86c112b2d5022445f505bf5
Runis <stntlr@gmail.com>
2014-10-19 00:18:51 -0700

    Added settings/photo

--- resources/views/settings/photo.blade.php
+++ resources/views/settings/photo.blade.php
@@ -4,9 +4,16 @@
 							@lang('settings.photo.current')
 						</h3>
 						<div class='well well-sm'>
-
+							{!!\Image::userPhoto(\Auth::user()->id)!!}
 						</div>
 						<h3>
 							@lang('settings.photo.change')
 						</h3>
+						<form action='' class='form-horizontal' method='post' role='form'>
+							<div class='form-group'>
+								<div class='col-lg-10'>
+									<textarea name='contents'></textarea>
+								</div>
+							</div>
+						</form>
 @stop
