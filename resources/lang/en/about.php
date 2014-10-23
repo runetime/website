a23119ce3d8a9e137148eac12322e160829f6c25
Runis <stntlr@gmail.com>
2014-10-22 22:26:14 -0700

    Put About page into a language file

---
+++ resources/lang/en/about.php
@@ -0,0 +1,32 @@
+<?php
+return [
+	'name' => "About Us",
+	'b1' => [
+		'p1'   => "RuneTime is a fansite that tributes to the wonderful world of RuneScape, an award winning MMORPG. Our ultimate goal is a strong, thriving and friendly community of like minded individuals that come together to have fun both in the game of RuneScape and outside of it. Being a part of many other RuneScape fansite communities, we've learned a lot about what is frustrating, annoying and what can kill a community pretty fast. RuneTime is starting fresh and from the beginning. We're going to do our best to avoid all these problems in the past we have encountered.",
+		'p2'   => "Just some of our features include:",
+		'p3'   => "Ads",
+		'p4'   => "What ads? Exactly! We don't think Ads should ever be apart of any community. We're not here to make money, we're here to enjoy each other with similar interest and ads just get in the way and make it less enjoyable. No one ever comes on and says, I love ads, please add them? To help cover costs we accept donations and anything that is given beyond our costs goes directly back to the community! Now what other fansite does that?",
+		'p5'   => "Radio",
+		'p6'   => "Our radio with chatbox engages our members with our very own DJ's who are members themselves. We are fully licensed and legal which is clearly visible on the radio page. All royalties are paid rightfully to the artists, songwriters and record labels. This is a great feature that brings the community together through music.",
+		'p7'   => "Forums",
+		'p8'   => "We offer a great and valuable way for players to communicate with each other and share their knowledge and experiences of RuneScape and life.",
+		'p9'   => "Full Disclosure",
+		'p10'  => "We hide nothing from our members. We fully disclose and discuss all matters with our members. We even post our finances, donations etc. for them to see where it all goes. We keep them updated on all the happenings of RuneTime as it's their community and they should know hows it's run and what is happening within it at all times. No censorship here! What other fansite does this?",
+		'p11'  => "Our Members",
+		'p12'  => "We actually only have real members. What does that mean? Well, the total members on the site may seem small compared to other sites for good reason. We don't allow bots, spammers, fake, locked or banned accounts to remain on RuneTime just to have an impressive member count. We don't care about the numbers, we care about having real members. Having a large number of accounts doesn't make a community, it's the people that do. We would rather have 20 active members than 200 spam accounts sitting idle.",
+		'p13'  => "Community Openness",
+		'p14'  => "From Day-One, we have been very open and welcoming to other communities and welcome friendly interactions. Unlike some fansites that are just about themselves and even censor and penalizing their members from even mentioning any other fansite by name (how silly is that?) we have an open arms policy. Even some of our Staff members are part of many different communities. We encourage community growth and that very much includes extending an arm to other communities and vice versa.",
+	],
+	'b2' => [
+		'name' => "How We are Setup",
+		'p1'   => "RuneTime has an Admin Team who look after the behind the scenes running of the site. We then have staff-members who contribute - such as DJ's, Forum Moderators, etc. Right now we have lots of openings for staff as we need to add a lot of content and add as much to the website as we can. The overall Team is shown here:",
+		'p2'   => "Also, the Admin team (consisting of the four 'Heads') are based in the USA and the UK - giving us plenty of coverage through many time-zones around the World. So one of us is probably always available if you need help or assistance - feel free to email or PM any of us. We are here to help!",
+	],
+	'b3' => [
+		'name' => "Security",
+		'p1'   => "We take security very seriously. Some of us have been 'burned' before, so we want to make RuneTime secure and safe from day-one. Our website stores all passwords using industry standard encryption. Passwords are never stored in plain-text. Personal information is stored on our secure severs, and we share NO information with other parties (unless required by law!). We do not sell, or pass-on to others, email addresses, user-names, or anything else.",
+		'p2'   => "We accept no Advertising and ensure all content is appropriate. We can and will ban users who act inappropriately. We do not tolerate any form of abuse, racism or behavior which is threatening or aggressive in any way. We understand that our users may be young children, teenagers and adults, of every nationality, gender and race, and we will ensure our website is sensitive to all these users. If anyone has a complaint, or sees something inappropriate, please forward it to one of the Runetime Heads for our immediate review.",
+		'p3'   => "Finally, we fully support the rules of Runescape, as listed here:",
+		'p4'   => "We do not support Real-World-Trading, selling Runescape Accounts, or any action which is against Jagex's rules. We do not support anyone who chooses to break these rules, and any links/messages/posting etc, which encourage rule-breaking will be removed immediately and the member warned. Ignoring warnings will result in account closure.",
+	],
+];
