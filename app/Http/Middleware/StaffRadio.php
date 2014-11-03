c056c79f39b26128c75b77a65745af2218c79e79
Runis <stntlr@gmail.com>
2014-11-03 10:51:24 -0800

    Added new staff Middleware

---
+++ app/Http/Middleware/StaffRadio.php
@@ -0,0 +1,24 @@
+<?php namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Routing\Middleware;
+
+class StaffRadio implements Middleware {
+
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{
+		if(!\Auth::check())
+			return \redirect::to('/login');
+		if(!\Auth::user()->hasOneOfRoles(1, 2, 3))
+			\App::abort(403);
+		return $next($request);
+	}
+
+}
