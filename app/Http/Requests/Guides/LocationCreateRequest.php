c7718b63a9eb3078f517f1049cc5df761f3fe48f
Runis <stntlr@gmail.com>
2014-11-05 09:51:01 -0800

    Created Quest Creation Requests

---
+++ app/Http/Requests/Guides/LocationCreateRequest.php
@@ -0,0 +1,33 @@
+<?php namespace App\Http\Requests\Guides;
+
+use App\Http\Requests\Request;
+/**
+ * Class LocationCreateRequest
+ * @package App\Http\Requests\Guides
+ */
+class LocationCreateRequest extends Request {
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		return [
+			'name'     => 'required',
+			'contents' => 'required',
+		];
+	}
+
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
+
+}
---
+++ app/Http/Requests/Guides/QuestCreateRequest.php
@@ -0,0 +1,45 @@
+<?php namespace App\Http\Requests\Guides;
+
+use App\Http\Requests\Request;
+/**
+ * Class QuestCreateRequest
+ * @package App\Http\Requests\Guides
+ */
+class QuestCreateRequest extends Request {
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		return [
+			'name'               => 'required',
+			'difficulty'         => 'required',
+			'length'             => 'required',
+			'membership'         => 'required',
+			'qp'                 => 'required',
+			'completed'          => 'required',
+			'description'        => 'required',
+			'quest_requirements' => 'required',
+			'skill_requirements' => 'required',
+			'items_required'     => 'required',
+			'items_recommended'  => 'required',
+			'reward'             => 'required',
+			'starting_point'     => 'required',
+			'guide'              => 'required',
+		];
+	}
+
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
+
+}
