0e19384a640920dfcc4b8a21c0f2400b93369263
Runis <stntlr@gmail.com>
2014-11-05 11:38:18 -0800

    Fixed broken Blade include paths

--- resources/views/staff/list.blade.php
+++ resources/views/staff/list.blade.php
@@ -1,47 +1,47 @@
-@extends('...layouts.default')
+@extends('layouts.default')
 @section('contents')
 			<div class='wrapper'>
 @if(!empty($admins))
 				<h2>
 					{!!Link::color('Administrators','Administrator')!!}
 				</h2>
-@include('list.group',['staff'=>$admins])
+@include('staff.list.group',['staff'=>$admins])
 @endif
 @if(!empty($radio))
 				<h2>
 					{!!Link::color('DJ Team','Radio DJ')!!}
 				</h2>
-@include('list.group',['staff'=>$radio])
+@include('staff.list.group',['staff'=>$radio])
 @endif
 @if(!empty($media))
 				<h2>
 					{!!Link::color('Media Team','Media Team')!!}
 				</h2>
-@include('list.group',['staff'=>$media])
+@include('staff.list.group',['staff'=>$media])
 @endif
 @if(!empty($content))
 				<h2>
 					{!!Link::color('Content Team','Content Team')!!}
 				</h2>
-@include('list.group',['staff'=>$content])
+@include('staff.list.group',['staff'=>$content])
 @endif
 @if(!empty($webDev))
 				<h2>
 					{!!Link::color('Web Development Team','Web Developer')!!}
 				</h2>
-@include('list.group',['staff'=>$webDev])
+@include('staff.list.group',['staff'=>$webDev])
 @endif
 @if(!empty($community))
 				<h2>
 					{!!Link::color('Community Team','Community Team')!!}
 				</h2>
-@include('list.group',['staff'=>$community])
+@include('staff.list.group',['staff'=>$community])
 @endif
 @if(!empty($events))
 				<h2>
 					{!!Link::color('Events Team','Events Team')!!}
 				</h2>
-@include('list.group',['staff'=>$events])
+@include('staff.list.group',['staff'=>$events])
 @endif
 			</div>
 @stop
