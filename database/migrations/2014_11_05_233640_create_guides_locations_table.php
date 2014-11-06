776e2a55197d50c94dc28f69c475c3723babb2fc
Runis <stntlr@gmail.com>
2014-11-06 14:04:07 -0800

    Added Location guide schema migration

---
+++ database/migrations/2014_11_05_233640_create_guides_locations_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+/**
+ * Class CreateGuidesLocationsTable
+ */
+class CreateGuidesLocationsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('guides_locations', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name');
+			$table->integer('author_id');
+			$table->mediumText('editors');
+			$table->mediumText('contents');
+			$table->mediumText('contents_parsed');
+			$table->timestamps();
+			$table->softDeletes();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('guides_locations');
+	}
+
+}
