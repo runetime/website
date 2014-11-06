9021a8204bcd0e53281583d433498aa47a998e87
Runis <stntlr@gmail.com>
2014-11-06 15:12:49 -0800

    Finished Database controller

---
+++ resources/views/databases/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.default')
+@section('contents')
+			<div class='wrapper'>
+				<h1>
+					Databases
+				</h1>
+				<ul>
+					<li>
+						<a href='/databases/items'>
+							Items Database
+						</a>
+					</li>
+					<li>
+						<a href='/databases/monsters'>
+							Monsters Database
+						</a>
+					</li>
+				</ul>
+			</div>
+@stop
