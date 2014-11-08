cd4180f1082664651032844ec26323aef8795c34
Runis <stntlr@gmail.com>
2014-11-08 12:06:01 -0800

    Finished Clan page

---
+++ resources/views/clan/index.blade.php
@@ -0,0 +1,88 @@
+@extends('layouts.default')
+@section('contents')
+			<div class='wrapper-dark'>
+				<img src='/img/clan/header.png' class='img-responsive center-block' alt='RuneTime Clan' />
+			</div>
+			<div class='wrapper'>
+				<h1>
+					Welcome to Our Clan Page!
+				</h1>
+				<p class='text-leading'>
+					This page will include information on how to join our clan, clan events, and other cool stuff related to our clan!
+				</p>
+				<h3>
+					How to Join
+				</h3>
+				<p>
+					Joining the RuneTime clan has no requirements so anyone can join, even F2P! Simply PM one of our staff members on RuneScape and they will be happy to refer you to a clan member who will then add you to the site, the best people to add would be <b>Static RS</b>, <b>Rune T I M E </b> and <b>DJ Serpent</b> as these people are all active RuneScape players who are in the clan.
+				</p>
+				<h3>
+					Important Information
+				</h3>
+				<p>
+					In the RuneTime clan we want to have as much fun as possible so we will always be planning events and activities, however there are a few rules which we believe will keep the clan happy and friendly if they are stuck too. Below you can find the rules and what will happen if they are broken!
+				</p>
+				<dl>
+					<dt>
+						Spamming
+					</dt>
+					<dd>
+						Warning
+					</dd>
+					<dt>
+						Flaming
+					</dt>
+					<dd>
+						Warning
+					</dd>
+					<dt>
+						Excessively Arguing
+					</dt>
+					<dd>
+						Warning
+					</dd>
+					<dt>
+						Putting Down the Clan or Website
+					</dt>
+					<dd>
+						Temporary Ban
+					</dd>
+					<dt>
+						3 Warnings
+					</dt>
+					<dd>
+						Temporary Ban
+					</dd>
+					<dt>
+						6 Warning
+					</dt>
+					<dd>
+						Temporary Ban Again
+					</dd>
+					<dt>
+						9 Warnings
+					</dt>
+					<dd>
+						Ban
+					</dd>
+					<dt>
+						Racism
+					</dt>
+					<dd>
+						Ban
+					</dd>
+					<dt>
+						Sexism
+					</dt>
+					<dd>
+						Ban
+					</dd>
+					<dt>
+						Any Other Form of Discrimination
+					</dt>
+					<dd>
+						Ban
+					</dd>
+				</dl>
+			</div>
+@stop
