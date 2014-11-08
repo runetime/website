ca67df2a30fa9c4342e0e919bb2cab78cb2052af
Runis <stntlr@gmail.com>
2014-11-08 14:24:18 -0800

    Added getByUser() to Radio Request repository

--- app/RuneTime/Radio/RequestRepository.php
+++ app/RuneTime/Radio/RequestRepository.php
@@ -12,4 +12,15 @@ class RequestRepository extends EloquentRepository {
 	public function __construct(Request $model) {
 		$this->model = $model;
 	}
+
+	/**
+	 * @param $id
+	 *
+	 * @return mixed
+	 */
+	public function getByUser($id) {
+		return $this->model->
+			where('author_id', '=', $id)->
+			get();
+	}
 }
