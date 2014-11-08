fa33ed69afd5c8e4b820d3a93c78ec4d2e3aa097
Runis <stntlr@gmail.com>
2014-11-08 12:35:54 -0800

    Added a Request form for Radio requesting

---
+++ app/Http/Requests/Radio/RequestSong.php
@@ -0,0 +1,33 @@
+<?php namespace App\Http\Requests\Radio;
+
+use App\Http\Requests\Request;
+/**
+ * Class RequestSong
+ * @package App\Http\Requests\Radio
+ */
+class RequestSong extends Request {
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		return [
+			'artist' => 'required',
+			'name' => 'required',
+		];
+	}
+
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
+
+}
