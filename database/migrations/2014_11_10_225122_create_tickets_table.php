e9a1551511078d7534cc69de9bb47b95d0158484
Runis <stntlr@gmail.com>
2014-11-10 15:20:21 -0800

    Added `tickets` and `post_ticket` migrations

---
+++ database/migrations/2014_11_10_225122_create_tickets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTicketsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('tickets', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('author_id');
+			$table->string('name');
+			$table->integer('posts_count');
+			$table->integer('last_post');
+			$table->integer('status');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('tickets');
+	}
+
+}
---
+++ database/migrations/2014_11_10_230543_create_post_ticket_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostTicketTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('post_ticket', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('post_id');
+			$table->integer('ticket_id');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('post_ticket');
+	}
+
+}
