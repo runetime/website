19a76f75b3c48e2771edd2a25a7c7cf35ba06d0c
Runis <stntlr@gmail.com>
2014-11-10 15:22:09 -0800

    Added posts() and addPost() methods to Ticket model

--- app/RuneTime/Tickets/Ticket.php
+++ app/RuneTime/Tickets/Ticket.php
@@ -1,18 +1,31 @@
 <?php
 namespace App\RuneTime\Tickets;
+use App\RuneTime\Forum\Threads\Post;
 use App\Runis\Core\Entity;
 class Ticket extends Entity {
 	protected $table = 'tickets';
-	protected $fillable = ['author_id', 'name', 'contents', 'contents_parsed'];
+	protected $fillable = ['author_id', 'name', 'posts_count', 'last_post', 'status'];
 	protected $dates = [];
 	protected $softDelete = true;
 	const STATUS_OPEN = 0;
 	const STATUS_CLOSED = 1;
+	const PER_PAGE = 20;
 
 	/**
 	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
 	 */
 	public function author() {
-		return $this->belongsTo('RT\Accounts\User', 'author_id');
+		return $this->belongsTo('App\Runis\Accounts\User', 'author_id');
+	}
+
+	public function posts() {
+		return $this->belongsToMany('App\RuneTime\Forum\Threads\Post');
+	}
+
+	/**
+	 * @param Post $post
+	 */
+	public function addPost(Post $post) {
+		$this->posts()->attach([$post->id]);
 	}
 }
