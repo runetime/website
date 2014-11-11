1cd4920d59a8212c3b61a09abbdf2b982c6d01fe
Runis <stntlr@gmail.com>
2014-11-10 16:10:02 -0800

    Changed the post _edit form to use a form action based off a given URL

--- resources/views/forums/post/_edit.blade.php
+++ resources/views/forums/post/_edit.blade.php
@@ -2,19 +2,18 @@
 if(!isset($button)) $button = 'Post';
 if(!isset($url))    $url='/forums/reply';
 ?>
-<form action='{{$url}}' class='reply' method='post'>
-	<input type='hidden' name='id' value='{{$id}}' />
+<form action='{{ $url }}' class='reply' method='post'>
 	<div class='row'>
 		<div class='hidden-xs col-sm-3 col-md-2 col-lg-1'>
-			{!!\Image::userPhoto(\Auth::user()->id, ['img-rounded'])!!}
+			{!! \Image::userPhoto(\Auth::user()->id, ['img-rounded']) !!}
 		</div>
 		<div class='col-xs-12 col-sm-9 col-md-10 col-lg-11'>
-			<textarea name='contents' id='contents' rows='15' title='Post' class='form-control'></textarea>
+			<textarea name='contents' id='contents' rows='15' class='form-control'></textarea>
 			<p>
 				<ul class='list-inline'>
 					<li>
 						<button class='btn btn-primary' type='submit'>
-							{{$button}}
+							{{ $button }}
 						</button>
 					</li>
 					<li>
