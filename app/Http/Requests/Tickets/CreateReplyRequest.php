acdc9ac25a750fcf77a3cc85677dc5d290f7eae1
Runis <stntlr@gmail.com>
2014-11-10 16:24:16 -0800

    Added CreateReply request for Tickets

---
+++ app/Http/Requests/Tickets/CreateReplyRequest.php
@@ -0,0 +1,29 @@
+<?php namespace App\Http\Requests\Tickets;
+
+use App\Http\Requests\Request;
+
+class CreateReplyRequest extends Request {
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		return [
+			'contents' => '',
+		];
+	}
+
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
+
+}
