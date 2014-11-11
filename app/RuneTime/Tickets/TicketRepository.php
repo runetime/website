bf516a231f6bc2a87ae11786f5946ca67207615b
Runis <stntlr@gmail.com>
2014-11-10 16:09:27 -0800

    Finished the Ticket repository

--- app/RuneTime/Tickets/TicketRepository.php
+++ app/RuneTime/Tickets/TicketRepository.php
@@ -5,4 +5,34 @@ class TicketRepository extends EloquentRepository {
 	public function __construct(Ticket $model) {
 		$this->model = $model;
 	}
+
+	/**
+	 * @param $statusId
+	 *
+	 * @return mixed
+	 */
+	public function getAllByStatus($statusId) {
+		return $this->model->
+			where('status', '=', $statusId)->
+			get();
+	}
+
+	/**
+	 * @param int $amount
+	 * @param     $statusId
+	 *
+	 * @return mixed
+	 */
+	public function getXByStatus($amount = Ticket::PER_PAGE, $statusId) {
+		return $this->model->
+			where('status', '=', $statusId)->
+			take($amount)->
+			get();
+	}
+
+	public function getByAuthor($id) {
+		return $this->model->
+			where('author_id', '=', $id)->
+			get();
+	}
 }
