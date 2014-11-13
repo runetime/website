39c1ba240e0a7f720c6565aa43b18158bf342c24
Runis <stntlr@gmail.com>
2014-11-13 13:20:05 -0800

    Added isLeader() method to User model

--- app/Runis/Accounts/User.php
+++ app/Runis/Accounts/User.php
@@ -118,6 +118,10 @@ class User extends Entity implements AuthenticatableContract, CanResetPasswordCo
 		return $this->hasOneOfRoles(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
 	}
 
+	public function isLeader() {
+		return $this->hasOneOfRoles(1, 2, 4, 6, 8, 10, 12);
+	}
+
 	/**
 	 * @return bool
 	 */
