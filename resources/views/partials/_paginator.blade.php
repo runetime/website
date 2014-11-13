fc5d840ee630ded2537753d97eaf3da1cb73db25
Runis <stntlr@gmail.com>
2014-11-13 12:06:24 -0800

    Updated paginator to use $url given

--- resources/views/partials/_paginator.blade.php
+++ resources/views/partials/_paginator.blade.php
@@ -1,18 +1,18 @@
 					<ul class='pagination'>
 						<li>
-							<a href='/forums/thread/{{ \String::slugEncode($thread->id, $thread->title) }}'>
+							<a href='{{ $url }}'>
 								<i class='fa fa-angle-double-left'></i>
 							</a>
 						</li>
 @for($pageX = 1; $pageX <= $pages; $pageX++)
 						<li{!! $pageX !== $page ? "" : " class='current'" !!}>
-							<a href='/forums/thread/{{ \String::slugEncode($thread->id, $thread->title) }}/page={{ $pageX }}'>
+							<a href='{{ $url }}/page={{ $pageX }}'>
 								{{ $pageX }}
 							</a>
 						</li>
 @endfor
 						<li>
-							<a href='/forums/thread/{{ \String::slugEncode($thread->id, $thread->title) }}/page={{ $pages }}'>
+							<a href='{{ $url }}/page={{ $pages }}'>
 								<i class='fa fa-angle-double-right'></i>
 							</a>
 						</li>
