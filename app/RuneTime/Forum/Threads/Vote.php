e4f0b58882ba755804981dcea2b98660db2e7fb0
Runis <stntlr@gmail.com>
2014-11-15 13:09:27 -0800

    Added Vote model and repository

---
+++ app/RuneTime/Forum/Threads/Vote.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\RuneTime\Forum\Threads;
+use App\Runis\Core\Entity;
+class Vote extends Entity {
+	protected $table = 'forum_votes';
+	protected $with = [];
+	protected $fillable = ['author_id', 'post_id', 'status'];
+	protected $dates = [];
+	protected $softDelete = false;
+	const STATUS_DOWN = 0;
+	const STATUS_NEUTRAL = 1;
+	const STATUS_UP = 2;
+
+	/**
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
+	public function post() {
+		return $this->belongsTo('App\RuneTime\Forum\Threads\Post', 'post_id');
+	}
+
+	/**
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
+	public function author() {
+		return $this->belongsTo('App\Runis\Accounts\User', 'author_id');
+	}
+}
---
+++ app/RuneTime/Forum/Threads/VoteRepository.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\RuneTime\Forum\Threads;
+use App\Runis\Core\EloquentRepository;
+class VoteRepository extends EloquentRepository {
+	/**
+	 * @param Vote $model
+	 */
+	public function __construct(Vote $model) {
+		$this->model = $model;
+	}
+
+	public function getByPost($id) {
+		return $this->model->
+			where('author_id', '=', \Auth::user()->id)->
+			where('post_id', '=', $id)->
+			first();
+	}
+}
