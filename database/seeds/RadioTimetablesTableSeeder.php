da28d8227826536a974b803fa766612033a67e7e
Runis <stntlr@gmail.com>
2014-11-15 14:18:03 -0800

    Fixed Timetable seeder to seed all 24 hours instead of 23

--- database/seeds/RadioTimetablesTableSeeder.php
+++ database/seeds/RadioTimetablesTableSeeder.php
@@ -5,7 +5,7 @@ class RadioTimetablesTableSeeder extends Seeder{
 		DB::table('radio_timetables')->delete();
 		for($a = 2014; $a < 2016; $a++) {
 			for($j = 1; $j < 366; $j++) {
-				for($x = 0; $x < 23; $x++) {
+				for($x = 0; $x <= 23; $x++) {
 					DB::table('radio_timetables')->
 						insertGetId([
 							'dj_id' => -1,
