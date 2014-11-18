2c6fb6277a085846933b35c13011c1032f1929de
Runis <stntlr@gmail.com>
2014-11-17 21:12:11 -0800

    Updated forums scss to account for post upvote/downvote for longer posts

--- resources/views/forums/post/_show.blade.php
+++ resources/views/forums/post/_show.blade.php
@@ -9,89 +9,87 @@
 							<div class='clearfix'>
 							</div>
 						</div>
-						<div class='col-xs-12 col-sm-3 col-md-2 text-center'>
-							{!!$post->author->title!!}
-							<br />
-							{!!\Image::userPhoto($post->author->id)!!}
-							<br />
-							{!!\Link::colorRole($post->author->importantRole()->id)!!}
-							<br />
-							{{$post->author->posts_active}} posts
+						<div class='col-xs-12 post-data row row-flat'>
+							<div class='col-xs-12 col-sm-3 col-md-2 text-center'>
+								{!!$post->author->title!!}
+								<br />
+								{!!\Image::userPhoto($post->author->id)!!}
+								<br />
+								{!!\Link::colorRole($post->author->importantRole()->id)!!}
+								<br />
+								{{$post->author->posts_active}} posts
 	@if(!empty($post->author_info->location))
-							<br />
-							<span class='text-muted'>:</span> {{$post->author_info->location}}
+								<br />
+								<span class='text-muted'>:</span> {{$post->author_info->location}}
 	@endif
 	@if(!empty($post->author_info->rsn))
-							<br />
-							<span class='text-muted'>:</span> {{$post->author_info->rsn}}
+								<br />
+								<span class='text-muted'>:</span> {{$post->author_info->rsn}}
 	@endif
 	@if(!empty($post->author_info->allegiance))
-							<br />
-							<span class='text-muted'>:</span> {{$post->author_info->allegiance}}
+								<br />
+								<span class='text-muted'>:</span> {{$post->author_info->allegiance}}
 	@endif
 	@if(!empty(json_decode($post->author->awards)))
-							<div class='post-awards'>
-								<a href='/awards/user/{{\String::slugEncode($post->author_info->id, $post->author_info->display_name)}}' title="View {{$post->author_info->display_name}}'s awards">
-									Awards
-								</a>
+								<div class='post-awards'>
+									<a href='/awards/user/{{\String::slugEncode($post->author_info->id, $post->author_info->display_name)}}' title="View {{$post->author_info->display_name}}'s awards">
+										Awards
+									</a>
 	@endif
-						</div>
-						<div class='col-xs-12 col-sm-9 col-md-10'>
-							<div class='post-contents' rt-data='post#{{ $post->id }}:contents'>
-								{!!$post->contents_parsed!!}
-								<div class='hidden' rt-data='post#{{ $post->id }}:source'>{!! $post->contents !!}</div>
 							</div>
+							<div class='col-xs-12 col-sm-9 col-md-10'>
+								<div class='post-contents' rt-data='post#{{ $post->id }}:contents'>
+									{!!$post->contents_parsed!!}
+									<div class='hidden' rt-data='post#{{ $post->id }}:source'>{!! $post->contents !!}</div>
+								</div>
 	@if(!empty($post->author_info->signature_parsed))
-							<hr />
-							{{$post->author_info->signature_parsed}}
+								<hr />
+								{{$post->author_info->signature_parsed}}
 	@endif
-							<div class='clearfix'>
-								<ul class='list-inline pull-left'>
+								<div class='clearfix'>
+									<ul class='list-inline pull-left'>
 	@if(\Auth::check())
 		@if(\Auth::user()->id != $post->author_id)
-									<li>
-										<a href='/forums/post/{{$post->id}}/report' title='Report This Post'>
-											Report
-										</a>
-									</li>
+										<li>
+											<a href='/forums/post/{{$post->id}}/report' title='Report This Post'>
+												Report
+											</a>
+										</li>
 		@endif
 		@if(\Auth::user()->isCommunity() || \Auth::user()->id == $post->author_id)
-									<li>
-										<a href='/forums/post/{{$post->id}}/edit' title='Edit This Post'>
-											Edit
-										</a>
-									</li>
+										<li>
+											<a href='/forums/post/{{$post->id}}/edit' title='Edit This Post'>
+												Edit
+											</a>
+										</li>
 		@endif
 		@if(\Auth::user()->isCommunity())
-									<li>
-										<a href='/forums/post/{{$post->id}}/status=0' title='Hide This Post'>
-											Hide
-										</a>
-									</li>
-									<li>
-										<a href='/forums/post/{{$post->id}}/delete' title='Delete This Post'>
-											Delete
-										</a>
-									</li>
+										<li>
+											<a href='/forums/post/{{$post->id}}/status=0' title='Hide This Post'>
+												Hide
+											</a>
+										</li>
+										<li>
+											<a href='/forums/post/{{$post->id}}/delete' title='Delete This Post'>
+												Delete
+											</a>
+										</li>
 		@endif
 		@if(\Auth::check())
-									<li>
-										<a title='Quote This Post' onclick="RuneTime.Forums.Post.quote({{ $post->id }});">
-											Quote
-										</a>
-									</li>
+										<li>
+											<a title='Quote This Post' onclick="RuneTime.Forums.Post.quote({{ $post->id }});">
+												Quote
+											</a>
+										</li>
 		@endif
 	@endif
-							</ul>
-							<div class='post-votes pull-right'>
+								</ul>
+								<div class='post-votes pull-right'>
 	@if(\Auth::check())
-								<div class='upvote'>
-									<i class='fa fa-arrow-up fa-2x'></i>
-								</div>
-								<div class='downvote'>
-									<i class='fa fa-arrow-down fa-2x'></i>
-								</div>
+                                    <i class='fa fa-arrow-up fa-2x upvote'></i>
+                                    <i class='fa fa-arrow-down fa-2x downvote'></i>
 	@endif
+								</div>
 							</div>
 						</div>
 					</div>
