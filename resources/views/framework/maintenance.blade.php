b4593d5c46ea4bfdd134d55f6ad1a1afc293241c
Runis <stntlr@gmail.com>
2014-11-18 11:47:36 -0800

    Added maintenance message

---
+++ resources/views/framework/maintenance.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.default')
+@section('contents')
+			<div class='wrapper'>
+				<h1>
+					Under Maintenance
+				</h1>
+				<p class='text-warning lead'>
+					We are currently under maintenance!
+				</p>
+				<p>
+					We'll be back as soon as we can.  In the meantime, we've probably sent out a message over at Twitter or Facebook!
+				</p>
+				<ul class='list-unstyled'>
+					<li>
+						<a href='https://www.facebook.com/RuneTimeOfficial'>
+							<img src='/img/fb.png' alt='Facebook' /> Facebook
+						</a>
+					</li>
+					<li>
+						<a href='https://twitter.com/Rune_Time'>
+							<img src='/img/tw.png' alt='Twitter' /> Twitter
+						</a>
+					</li>
+				</ul>
+			</div>
+@stop
