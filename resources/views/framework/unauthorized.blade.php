f2ab0045737f3e127cc716961a03ff97f41e9ece
Runis <stntlr@gmail.com>
2014-11-18 11:48:28 -0800

    Added 403 Unauthorized message

---
+++ resources/views/framework/unauthorized.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.default')
+@section('contents')
+			<div class='wrapper'>
+				<h1>
+					Unauthorized 403
+				</h1>
+				<p class='text-danger lead'>
+					It seems you are unauthorized from accessing this page!  Sorry about that :(
+				</p>
+			</div>
+@stop
