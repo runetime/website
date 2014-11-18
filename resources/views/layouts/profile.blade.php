d9070aeca85d6986e4b1e6e2565b2a403a5fb178
Runis <stntlr@gmail.com>
2014-11-18 11:51:03 -0800

    Centered profile image on Profiles

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -3,15 +3,15 @@
 			<div class='wrapper'>
 				<div class='row'>
 					<div class='col-xs-12 col-sm-3 col-md-2'>
-						{!!\Image::userPhoto($profile->id)!!}
+						{!! \Image::userPhoto($profile->id, ['center-block']) !!}
 						<ul class='list-group'>
-							<a href='/profile/{{\String::slugEncode($profile->id, $profile->display_name)}}' class='list-group-item'>
+							<a href='/profile/{{ \String::slugEncode($profile->id, $profile->display_name) }}' class='list-group-item'>
 								@lang('profile.nav.overview')
 							</a>
-							<a href='/profile/{{\String::slugEncode($profile->id, $profile->display_name)}}/feed' class='list-group-item'>
+							<a href='/profile/{{ \String::slugEncode($profile->id, $profile->display_name) }}/feed' class='list-group-item'>
 								@lang('profile.nav.profile_feed')
 							</a>
-							<a href='/profile/{{\String::slugEncode($profile->id, $profile->display_name)}}/friends' class='list-group-item'>
+							<a href='/profile/{{\String::slugEncode($profile->id, $profile->display_name) }}/friends' class='list-group-item'>
 								@lang('profile.nav.friends')
 							</a>
 						</ul>
