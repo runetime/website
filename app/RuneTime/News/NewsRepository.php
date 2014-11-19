f34ac203d143e576b7e533348e7ff21b9a5b93ca
Runis <stntlr@gmail.com>
2014-11-18 19:20:42 -0800

    Added documentation to NewsRepository methods

--- app/RuneTime/News/NewsRepository.php
+++ app/RuneTime/News/NewsRepository.php
@@ -2,9 +2,18 @@
 namespace App\RuneTime\News;
 use App\Runis\Core\EloquentRepository;
 class NewsRepository extends EloquentRepository{
+	/**
+	 * @param News $model
+	 */
 	public function __construct(News $model) {
 		$this->model = $model;
 	}
+
+	/**
+	 * @param int $count
+	 *
+	 * @return mixed
+	 */
 	public function getRecentNews($count = 5) {
 		return $this->model->
 			where('status', '=', News::STATUS_PUBLISHED)->
