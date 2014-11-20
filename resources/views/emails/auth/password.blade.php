9eb7a3cd40755462175192e9a5d3e715fdbf884e
Runis <stntlr@gmail.com>
2014-11-20 11:33:44 -0800

    Fixed password resets

--- resources/views/emails/auth/password.blade.php
+++ resources/views/emails/auth/password.blade.php
@@ -7,7 +7,7 @@
 		<h2>Password Reset</h2>
 
 		<div>
-			To reset your password, complete this form: {{ url('password/reset', [$token]) }}.<br/>
+			To reset your password, complete this form: <a href='http://178.62.54.243/password/reset/{{ $token }}'>http://178.62.54.243/password/reset/{{ $token }}</a><br/>
 
 			This link will expire in {{ config('auth.reminder.expire', 60) }} minutes.
 		</div>
