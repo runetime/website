4378450522037326be2fdcf470c48b7b9a5b191c
Runis <stntlr@gmail.com>
2014-11-23 12:13:19 -0800

    Moved compiled signup.ts file

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,9 +1,7 @@
-/*global $:false, jQuery:false */
-/*jslint browser: true, devel: true */
 $(function () {
-	"use strict";
-	setTimeout(function () {
-		RuneTime.FormSignup = new RuneTime.SignupForm();
-		RuneTime.FormSignup.setup();
-	}, 1000);
+    "use strict";
+    setTimeout(function () {
+        RuneTime.FormSignup = new RuneTime.SignupForm();
+        RuneTime.FormSignup.setup();
+    }, 1000);
 });
--- resources/assets/js/signup.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    "use strict";
-    setTimeout(function () {
-        RuneTime.FormSignup = new RuneTime.SignupForm();
-        RuneTime.FormSignup.setup();
-    }, 1000);
-});
