5347a179597625f5ef5201e679433c47449f9d55
Runis <stntlr@gmail.com>
2014-11-25 15:07:24 -0800

    Added environment check to Scheduling

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,7 +29,7 @@ class Kernel extends ConsoleKernel {
 	 */
 	protected function schedule(Schedule $schedule)
 	{
-		if(getenv('APP_ENV') !== "local")
+		if(getenv('APP_ENV') != 'local')
 			$schedule->command('radio:update')
 				->cron('*/1 * * * * *');
 		$schedule->command('signatures:update')->
