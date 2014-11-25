b4af056ce95819235fd218851045fcca11b39086
Runis <stntlr@gmail.com>
2014-11-25 12:28:21 -0800

    Updated signature type listings to use images instead of text

--- resources/views/signatures/type.blade.php
+++ resources/views/signatures/type.blade.php
@@ -1,8 +1,13 @@
 @extends('layouts.default')
 @section('contents')
-			<div class='wrapper'>
-				<a href='/signatures/username={{$username}}/type=stat'>
-					@lang('signature.type.stat')
-				</a>
+			<div class='wrapper row row-flat'>
+				<div class='col-xs-12 col-sm-6 col-md-4'>
+					<a href='/signatures/username={{ $username }}/type=stat'>
+						<h3 class='text-center'>
+							@lang('signature.type.stat')
+						</h3>
+						<img src='/signatures/hStatic;stat;wp69497ed6_06' class='center-block img-responsive' />
+					</a>
+				</div>
 			</div>
 @stop
