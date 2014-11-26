864b30d1469181ec22505e777800c609aed663af
Runis <stntlr@gmail.com>
2014-11-26 14:11:17 -0800

    Moved duplicate profile code to profile layout

--- resources/views/forums/profile/index.blade.php
+++ resources/views/forums/profile/index.blade.php
@@ -1,45 +1,5 @@
 @extends('layouts.profile')
 @section('profile')
-						<div class='row row-flat'>
-							<div class='col-xs-6 col-sm-5 col-md-3'>
-								<h1>
-									{{ $profile->display_name }}
-								</h1>
-								@lang('profile.overview.member_since', ['date' => \Time::DMY($profile->created_at)])
-								<br />
-								<span class='label label-success'>@lang('profile.status.online')</span> @lang('profile.overview.last_active')
-							</div>
-							<div class='col-xs-6 col-sm-7 col-md-9'>
-								<div class='well well-sm'>
-@if(!empty($status))
-									<p class='inline'>{!! $status->posts[0]->contents_parsed !!}</p>
-									<ul class='list-inline'>
-										<li>
-											<a href='/forums/statuses/{{ \String::slugEncode($status->id, 'by-', $status->author->display_name) }}'>
-												Posted {{ \Time::monthDay($status->created_at) }}
-											</a>
-										</li>
-										<li>
-											<a href='/forums/statuses/{{ \String::slugEncode($status->id, 'by-', $status->author->display_name) }}'>
-												{{ $status->reply_count }} replies
-											</a>
-										</li>
-									</ul>
-@else
-									<p>
-										<em>
-	@if(\Auth::check() && \Auth::user()->id === $profile->id)
-										You have not made a status.  <a href='/forums/statuses/create'>What's on your mind?</a>
-	@else
-											{{ $profile->display_name }} has not made a status.
-	@endif
-										</em>
-									</p>
-@endif
-								</div>
-							</div>
-						</div>
-						<br />
 						<div class='box'>
 							<h3>
 								@lang('profile.overview.about.name')
--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -17,6 +17,46 @@
 						</ul>
 					</div>
 					<div class='col-xs-12 col-sm-9 col-md-10'>
+						<div class='row row-flat'>
+							<div class='col-xs-6 col-sm-5 col-md-3'>
+								<h1>
+									{{ $profile->display_name }}
+								</h1>
+								@lang('profile.overview.member_since', ['date' => \Time::DMY($profile->created_at)])
+								<br />
+								<span class='label label-success'>@lang('profile.status.online')</span> @lang('profile.overview.last_active')
+							</div>
+							<div class='col-xs-6 col-sm-7 col-md-9'>
+								<div class='well well-sm'>
+@if(!empty($status))
+									<p class='inline'>{!! $status->posts[0]->contents_parsed !!}</p>
+									<ul class='list-inline'>
+										<li>
+											<a href='/forums/statuses/{{ \String::slugEncode($status->id, 'by-', $status->author->display_name) }}'>
+												Posted {{ \Time::monthDay($status->created_at) }}
+											</a>
+										</li>
+										<li>
+											<a href='/forums/statuses/{{ \String::slugEncode($status->id, 'by-', $status->author->display_name) }}'>
+												{{ $status->reply_count }} replies
+											</a>
+										</li>
+									</ul>
+@else
+									<p>
+										<em>
+	@if(\Auth::check() && \Auth::user()->id === $profile->id)
+										You have not made a status.  <a href='/forums/statuses/create'>What's on your mind?</a>
+	@else
+											{{ $profile->display_name }} has not made a status.
+	@endif
+										</em>
+									</p>
+@endif
+								</div>
+							</div>
+						</div>
+						<br />
 @yield('profile')
 					</div>
 				</div>
