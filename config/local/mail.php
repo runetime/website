cb4dfb31a13780fe5fd6660e2e476d6710b3dc55
Runis <stntlr@gmail.com>
2014-11-27 13:39:42 -0800

    Added mail pretend to local environment

---
+++ config/local/mail.php
@@ -0,0 +1,15 @@
+<?php
+return [
+	/*
+	|--------------------------------------------------------------------------
+	| Mail "Pretend"
+	|--------------------------------------------------------------------------
+	|
+	| When this option is enabled, e-mail will not actually be sent over the
+	| web and will instead be written to your application's logs files so
+	| you may inspect the message. This is great for local development.
+	|
+	*/
+
+	'pretend' => true,
+];
