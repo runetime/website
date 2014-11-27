659761fb901918a53c136f4367fbda250c925336
Runis <stntlr@gmail.com>
2014-11-26 16:16:45 -0800

    Minified compiled JS output

--- resources/assets/js/compile.bat
+++ resources/assets/js/compile.bat
@@ -1,9 +1,21 @@
 :: Modules
 
-call tsc --out ../../../public/js/compiled.js modules/calculator.ts modules/chatbox.ts modules/combatcalculator.ts modules/forums.ts modules/livestream.ts modules/namechecker.ts modules/notifications.ts modules/radio.ts modules/signup.ts modules/staff_list.ts modules/utilities.ts
+call tsc --out temp-admin.js modules/admin.ts
 
-call tsc --out ../../../public/js/admin.js modules/admin.ts
+call tsc --out temp-compiled.js modules/calculator.ts modules/chatbox.ts modules/combatcalculator.ts modules/forums.ts modules/livestream.ts modules/namechecker.ts modules/notifications.ts modules/radio.ts modules/signup.ts modules/staff_list.ts modules/utilities.ts
+
+
+:: UglifyJS
+
+call uglifyjs temp-admin.js -c -m -o ./../../../public/js/admin.js
+call uglifyjs temp-compiled.js -c -m -o ./../../../public/js/compiled.js
 
 
 :: Vendor
-uglifyjs vendor/jquery.js vendor/jquery-ui.js vendor/bootstrap.js vendor/jasny-bootstrap.js -c -m -o ../../../public/js/vendor.js
+call uglifyjs vendor/jquery.js vendor/jquery-ui.js vendor/bootstrap.js vendor/jasny-bootstrap.js -c -m -o ../../../public/js/vendor.js
+
+
+:: Remove temporary files
+
+call del .\temp-admin.js
+call del .\temp-compiled.js
