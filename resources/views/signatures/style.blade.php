4a051897d110cfbbd48dc7ed48de483283724a49
Runis <stntlr@gmail.com>
2014-11-26 16:54:30 -0800

    Reformatted syntax of signature style

--- resources/views/signatures/style.blade.php
+++ resources/views/signatures/style.blade.php
@@ -4,8 +4,8 @@
 				<div class='row'>
 @foreach($imgs as $img)
 					<div class='col-xs-12 col-sm-6 col-md-3'>
-						<a href='/signatures/username={{$username}}/type={{$type}}/style={{str_replace('.png','',$img)}}'>
-							<img src='/img/signatures/backgrounds/{{$img}}' />
+						<a href='/signatures/username={{ $username }}/type={{ $type }}/style={{ str_replace('.png', '', $img) }}'>
+							<img src='/img/signatures/backgrounds/{{ $img }}' />
 						</a>
 					</div>
 @endforeach
