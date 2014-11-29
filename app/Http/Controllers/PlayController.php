5dccaa673317ccf472132da283ed33dde7f88a51
Runis <stntlr@gmail.com>
2014-11-29 09:29:08 -0800

    Removed old chatbox JS includes and @get PHPDocs in PlayController

--- app/Http/Controllers/PlayController.php
+++ app/Http/Controllers/PlayController.php
@@ -1,9 +1,8 @@
 <?php
 namespace App\Http\Controllers;
+
 class PlayController extends BaseController {
 	/**
-	 * @get("play")
-	 *
 	 * @return \Illuminate\View\View
 	 */
 	public function getIndex() {
@@ -14,26 +13,20 @@ class PlayController extends BaseController {
 	}
 
 	/**
-	 * @get("play/3")
-	 *
 	 * @return \Illuminate\View\View
 	 */
 	public function get3() {
 		$this->bc(['play' => trans('play.index.play_runescape')]);
-		$this->js(['chatbox']);
 		$this->nav('navbar.runescape.runescape');
 		$this->title(trans('play.index.3'));
 		return $this->view('play.3');
 	}
 
 	/**
-	 * @get("play/osrs")
-	 *
 	 * @return \Illuminate\View\View
 	 */
 	public function getOSRS() {
 		$this->bc(['play' => trans('play.index.play_runescape')]);
-		$this->js(['chatbox']);
 		$this->nav('navbar.runescape.runescape');
 		$this->title(trans('play.index.osrs'));
 		return $this->view('play.osrs');
