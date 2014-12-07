89c55d93e95ff2ec8354be69c0de4a9a7ba997e4
Runis <stntlr@gmail.com>
2014-12-07 14:50:05 -0800

    Added home test

---
+++ tests/HomeTest.php
@@ -0,0 +1,12 @@
+<?php
+class HomeTest extends TestCase {
+	/**
+	 *
+	 */
+	public function testHomeIndex()
+	{
+		$response = $this->call('GET', '/');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+}
