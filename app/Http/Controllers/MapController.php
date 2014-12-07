550ecdc002e6ea262db2fb36debd427ccd700018
Runis <stntlr@gmail.com>
2014-12-07 15:44:08 -0800

    Fixed 502 error

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -10,7 +10,7 @@ class MapController extends BaseController {
 	 */
 	public function getIndex() {
 		$this->nav('navbar.runescape.runescape');
-		$this->title(lang('maps.title'));
+		$this->title(trans('maps.title'));
 		return $this->view('map.index');
 	}
 
@@ -20,7 +20,7 @@ class MapController extends BaseController {
 	public function getMembers() {
 		$this->bc(['map' => trans('maps.title')]);
 		$this->nav('navbar.social.social');
-		$this->title(lang('maps.members.title'));
+		$this->title(trans('maps.members.title'));
 		return $this->view('map.members');
 	}
 
@@ -30,7 +30,7 @@ class MapController extends BaseController {
 	public function getRunescape() {
 		$this->bc(['map' => trans('maps.title')]);
 		$this->nav('navbar.runescape.runescape');
-		$this->title(lang('maps.rs.title'));
+		$this->title(trans('maps.rs.title'));
 		return $this->view('map.runescape.index');
 	}
 
@@ -40,7 +40,7 @@ class MapController extends BaseController {
 	public function getRS3() {
 		$this->bc(['map' => 'Maps', 'map/runescape' => trans('maps.rs.title')]);
 		$this->nav('navbar.runescape.runescape');
-		$this->title(lang('maps.rs.rs3'));
+		$this->title(trans('maps.rs.rs3'));
 		return $this->view('map.runescape.3');
 	}
 
@@ -48,9 +48,9 @@ class MapController extends BaseController {
 	 * @return \Illuminate\View\View
 	 */
 	public function getOS() {
-		$this->bc(['map' => lang('maps.title'), 'map/runescape' => lang('maps.rs.title')]);
+		$this->bc(['map' => trans('maps.title'), 'map/runescape' => trans('maps.rs.title')]);
 		$this->nav('navbar.runescape.runescape');
-		$this->title(lang('maps.rs.osrs'));
+		$this->title(trans('maps.rs.osrs'));
 		return $this->view('map.runescape.old');
 	}
 }
