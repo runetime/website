b7513904bd59c77ead759fcbdae430ff41bb9ab4
Runis <stntlr@gmail.com>
2014-12-07 14:44:55 -0800

    Added Donate test

---
+++ tests/DonateTest.php
@@ -0,0 +1,12 @@
+<?php
+class DonateTest extends TestCase {
+	/**
+	 *
+	 */
+	public function testIndex()
+	{
+		$response = $this->call('GET', 'donate');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+}
