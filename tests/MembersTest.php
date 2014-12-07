c0093fcca42529c00266f096dc2ffa72515ba56b
Runis <stntlr@gmail.com>
2014-12-07 15:49:40 -0800

    Added Members tests

---
+++ tests/MembersTest.php
@@ -0,0 +1,32 @@
+<?php
+class MembersTest extends TestCase {
+	/**
+	 *
+	 */
+	public function testIndex()
+	{
+		$response = $this->call('GET', 'members');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	/**
+	 *
+	 */
+	public function testSearch()
+	{
+		$response = $this->call('GET', 'members/role=a/prefix=b/order=c');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	/**
+	 *
+	 */
+	public function testPage()
+	{
+		$response = $this->call('GET', 'members/role=a/prefix=b/order=c/page=1');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+}
