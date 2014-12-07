72c51b598fe026442807bc9499769611459ed252
Runis <stntlr@gmail.com>
2014-12-07 14:27:20 -0800

    Fixed password_resets migration

---
+++ tests/AwardTest.php
@@ -0,0 +1,23 @@
+<?php
+
+class AwardTest extends TestCase {
+	/**
+	 *
+	 */
+	public function testAwardsIndex()
+	{
+		$response = $this->call('GET', 'awards');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	/**
+	 *
+	 */
+	public function testAwardsView()
+	{
+		$response = $this->call('GET', 'awards/1-test');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+}
