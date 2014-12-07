f5da020ed9c0f597282f5e707054040a408a4324
Runis <stntlr@gmail.com>
2014-12-07 14:56:18 -0800

    Added Legal test

---
+++ tests/LegalTest.php
@@ -0,0 +1,32 @@
+<?php
+class LegalTest extends TestCase {
+	/**
+	 *
+	 */
+	public function testLegal()
+	{
+		$response = $this->call('GET', 'legal/english');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	/**
+	 *
+	 */
+	public function testPrivacy()
+	{
+		$response = $this->call('GET', 'privacy');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	/**
+	 *
+	 */
+	public function testTerms()
+	{
+		$response = $this->call('GET', 'terms');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+}
