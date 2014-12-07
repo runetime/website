a26e36a2624a3500a6da5d49086a152bed7c5150
Runis <stntlr@gmail.com>
2014-12-07 15:45:12 -0800

    Added Map test

---
+++ tests/MapTest.php
@@ -0,0 +1,42 @@
+<?php
+class MapTest extends TestCase {
+	/**
+	 *
+	 */
+	public function testIndex()
+	{
+		$response = $this->call('GET', 'map');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	/**
+	 *
+	 */
+	public function testRS()
+	{
+		$response = $this->call('GET', 'map/runescape');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	/**
+	 *
+	 */
+	public function testRS3()
+	{
+		$response = $this->call('GET', 'map/runescape/3');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	/**
+	 *
+	 */
+	public function testRSOS()
+	{
+		$response = $this->call('GET', 'map/runescape/old-school');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+}
