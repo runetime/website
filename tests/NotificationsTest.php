e28b74fa577c41f1e1ef41ce1fd239c1431953c5
Runis <stntlr@gmail.com>
2014-12-07 16:17:42 -0800

    Added Notifications tests

---
+++ tests/NotificationsTest.php
@@ -0,0 +1,32 @@
+<?php
+class NotificationsTest extends TestCase {
+	/**
+	 *
+	 */
+	public function testIndex()
+	{
+		$this->login();
+		$response = $this->call('GET', 'notifications');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	/**
+	 *
+	 */
+	public function testView()
+	{
+		$this->login();
+		$response = $this->call('GET', 'notifications/1-at-123');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	public function testSetAllRead()
+	{
+		$this->login();
+		$response = $this->call('GET', 'notifications/set-all-read');
+
+		$this->assertEquals(302, $response->getStatusCode());
+	}
+}
