ee5acef0b711a966226425f2607dc52787abc5af
Runis <stntlr@gmail.com>
2014-12-07 16:43:30 -0800

    Added Status tests

---
+++ tests/StatusTest.php
@@ -0,0 +1,62 @@
+<?php
+use App\RuneTime\Statuses\Status;
+
+class StatusTest extends TestCase {
+	/**
+	 *
+	 */
+	public function testIndex()
+	{
+		$response = $this->call('GET', 'forums/statuses');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	/**
+	 *
+	 */
+	public function testGetCreate()
+	{
+		$this->login();
+		$response = $this->call('GET', 'forums/statuses/create');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	/**
+	 *
+	 */
+	public function testPostCreate()
+	{
+		$this->login();
+		$data = [
+			'contents' => 'contents',
+		];
+		$response = $this->call('POST', 'forums/statuses/create', $data);
+
+		$this->assertEquals(302, $response->getStatusCode());
+	}
+
+	/**
+	 *
+	 */
+	public function testView()
+	{
+		$status = Status::orderBy('created_at', 'desc')->first();
+		$response = $this->call('GET', 'forums/statuses/' . $status->id . '-by-' . $status->author->display_name);
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+
+	/**
+	 *
+	 */
+	public function testReply()
+	{
+		$status = Status::orderBy('created_at', 'desc')->first();
+		$response = $this->call('POST', 'forums/statuses/' . $status->id . '-by-' . $status->author->display_name . '/reply');
+
+		$this->assertEquals(302, $response->getStatusCode());
+	}
+}
