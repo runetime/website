ba092a9eaf147dd5e661fb61aa36cdbcf44fbe2c
Runis <stntlr@gmail.com>
2014-12-07 16:47:07 -0800

    Fixed Markdown page

--- app/Http/Controllers/TransparencyController.php
+++ app/Http/Controllers/TransparencyController.php
@@ -17,7 +17,7 @@ class TransparencyController extends BaseController {
 	 * @return \Illuminate\View\View
 	 */
 	public function getMarkdown() {
-		$paths = \File::allFiles(\base_path() . '/resources/views/parsedown');
+		$paths = \File::allFiles(\base_path('views/parsedown'));
 		$files = [];
 		foreach($paths as $path) {
 			$name = str_replace("_", " ", $path);
