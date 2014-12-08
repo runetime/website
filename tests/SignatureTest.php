d1859739eacb392e9c22bae6b9fa63cea52247c4
Runis <stntlr@gmail.com>
2014-12-07 16:32:29 -0800

    Added Signature tests

---
+++ tests/SignatureTest.php
@@ -0,0 +1,46 @@
+<?php
+
+class SignatureTest extends TestCase {
+	/**
+	 *
+	 */
+	public function testIndex()
+	{
+		$response = $this->call('GET', 'signatures');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	/**
+	 *
+	 */
+	public function testPostUsername()
+	{
+		$form = [
+			'username' => 'zezima',
+		];
+		$response = $this->call('POST', 'signatures', $form);
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	/**
+	 *
+	 */
+	public function testStyle()
+	{
+		$response = $this->call('GET', 'signatures/username=zezima/type=stat');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	/**
+	 *
+	 */
+	public function testFinal()
+	{
+		$response = $this->call('GET', 'signatures/username=zezima/type=stat/style=wp061ca5fe_06');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+}
