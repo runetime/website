e0c0a4280b2a1ebeed686d396dc532996670097a
Runis <stntlr@gmail.com>
2014-12-07 16:22:01 -0800

    Added Profile tests

---
+++ tests/ProfileTest.php
@@ -0,0 +1,26 @@
+<?php
+use App\Runis\Accounts\User;
+
+class ProfileTest extends TestCase {
+	/**
+	 *
+	 */
+	public function testIndex()
+	{
+		$user = User::orderBy('created_at', 'desc')->first();
+		$response = $this->call('GET', 'profile/' . $user->id . '-' . $user->display_name);
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	/**
+	 *
+	 */
+	public function testFeed()
+	{
+		$user = User::orderBy('created_at', 'desc')->first();
+		$response = $this->call('GET', 'profile/' . $user->id . '-' . $user->display_name . '/feed');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+}
