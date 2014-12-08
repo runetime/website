041844a1ea07ad6930968653c951ffe84bca53bb
Runis <stntlr@gmail.com>
2014-12-07 16:46:56 -0800

    Added Transparency tests

---
+++ tests/TransparencyTest.php
@@ -0,0 +1,22 @@
+<?php
+class TransparencyTest extends TestCase {
+	/**
+	 *
+	 */
+	public function testIndex()
+	{
+		$response = $this->call('GET', 'transparency');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	/**
+	 *
+	 */
+	public function testMarkdown()
+	{
+		$response = $this->call('GET', 'transparency/markdown');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+}
