46a7e1e9d0acfe15709538d2fd4d1f8a3e11a846
Runis <stntlr@gmail.com>
2014-12-08 23:40:07 -0800

    Finished Gulp setup

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,32 +1,73 @@
-var elixir = require('laravel-elixir');
+var gulp = require('gulp');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+var concat = require('gulp-concat');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var uglifyjs = require('gulp-uglifyjs');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var ts = require('gulp-typescript');
 
-elixir(function(mix) {
-    mix.sass('app.scss')
-       .publish(
-            'jquery/dist/jquery.min.js',
-            'public/js/vendor/jquery.js'
-        )
-       .publish(
-            'bootstrap-sass-official/assets/javascripts/bootstrap.js',
-            'public/js/vendor/bootstrap.js'
-        )
-       .publish(
-            'font-awesome/css/font-awesome.min.css',
-            'public/css/vendor/font-awesome.css'
-        )
-       .publish(
-            'font-awesome/fonts',
-            'public/css/fonts'
-        );
+var paths = {
+	'assets': {
+		scss: './assets/scss/*.scss',
+		typescript: './assets/typescript/modules/*.ts'
+	},
+	'public': {
+		css: './public/css',
+		js: './public/js'
+	}
+};
+
+// Sass Compile Task
+gulp.task('scss', function() {
+	return gulp.src(paths.assets.scss)
+		.pipe(sass({ style: 'compressed' }))
+		.pipe(gulp.dest(paths.public.css));
 });
+
+// Scripts-Admin Task
+gulp.task('scripts-admin', function() {
+	var tsResult = gulp.src('./assets/typescript/admin/*.ts')
+		.pipe(sourcemaps.init())
+		.pipe(ts({
+			sortOutput: true
+		}));
+
+	return tsResult.js
+		.pipe(concat('admin.js'))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('./public/js/'));
+});
+
+// Scripts-Modules Task
+gulp.task('scripts-modules', function() {
+	var tsResult = gulp.src('./assets/typescript/modules/*.ts')
+		.pipe(sourcemaps.init())
+		.pipe(ts({
+			sortOutput: true
+		}));
+
+	return tsResult.js
+		.pipe(concat('modules.js'))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('./public/js'));
+});
+
+// Scripts-Admin Task
+gulp.task('scripts-vendor', function() {
+	gulp.src('./assets/typescript/vendor/*.js')
+		.pipe(uglifyjs('vendor.js', {
+			outSourceMap: false
+		}))
+		.pipe(gulp.dest('./public/js'))
+});
+
+gulp.task('watch', function() {
+	gulp.watch('./assets/typescripts/*', ['scripts-admin', 'scripts-modules', 'scripts-vendor']);
+	gulp.watch('./assets/scss/*.scss', ['scss']);
+	gulp.watch('./assets/scss/partials/*.scss', ['scss']);
+});
+
+gulp.task('default', ['cass']);
