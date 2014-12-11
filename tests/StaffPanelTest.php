d71697131dff7706075949de2f3d56f68a653e09
Runis <stntlr@gmail.com>
2014-12-11 11:17:44 -0800

    Added Staff Panel test

---
+++ tests/StaffPanelTest.php
@@ -0,0 +1,72 @@
+<?php
+class StaffPanelTest extends TestCase {
+	/**
+	 *
+	 */
+	public function testIndex()
+	{
+		$this->login();
+		$response = $this->call('GET', 'staff');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	/**
+	 *
+	 */
+	public function testUserReport()
+	{
+		$this->login();
+		$user = \App\Runis\Accounts\User::find(1);
+		$data = [
+			'username' => $user->display_name,
+			'reason'   => 'Staff Panel User Report Test',
+		];
+		$response = $this->call('POST', 'staff/report', $data);
+
+		$this->assertEquals(302, $response->getStatusCode());
+	}
+
+	/**
+	 *
+	 */
+	public function testUserMute()
+	{
+		$this->login();
+		$user = \App\Runis\Accounts\User::find(1);
+		$data = [
+			'username' => $user->display_name,
+			'reason'   => 'Staff Panel User Mute Test',
+		];
+		$response = $this->call('POST', 'staff/mute', $data);
+
+		$this->assertEquals(302, $response->getStatusCode());
+	}
+
+	/**
+	 *
+	 */
+	public function testGetCheckup()
+	{
+		$this->login();
+		$response = $this->call('GET', 'staff/checkup');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	/**
+	 *
+	 */
+	public function testPostCheckup()
+	{
+		$this->login();
+		$data = [
+			'hours_active' => 7,
+			'active'       => 0,
+			'team'         => 'development',
+		];
+		$response = $this->call('POST', 'staff/checkup', $data);
+
+		$this->assertEquals(302, $response->getStatusCode());
+	}
+}
