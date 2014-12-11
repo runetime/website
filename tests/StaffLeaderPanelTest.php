fb9b16c15f6721dae44e5f124afc09fbcd9fd4e6
Runis <stntlr@gmail.com>
2014-12-11 14:52:33 -0800

    Added team leader tests, last was controller

---
+++ tests/StaffLeaderPanelTest.php
@@ -0,0 +1,45 @@
+<?php
+class StaffLeaderPanelTest extends TestCase {
+	/**
+	 *
+	 */
+	public function testIndex()
+	{
+		$this->login();
+		$response = $this->call('GET', 'staff/leader');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	public function testDemoteStaff()
+	{
+		$this->login();
+		$response = $this->call('GET', 'staff/leader');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	public function testTempBan()
+	{
+		$this->login();
+		$response = $this->call('GET', 'staff/leader');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	public function testMuteUser()
+	{
+		$this->login();
+		$response = $this->call('GET', 'staff/leader');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	public function testClearChatbox()
+	{
+		$this->login();
+		$response = $this->call('GET', 'staff/leader');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+}
