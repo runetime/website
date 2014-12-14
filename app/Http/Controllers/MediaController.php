1d50f0b7922f3e7364f1f07eb2e41506f20c454e
Runis <stntlr@gmail.com>
2014-12-14 03:08:59 -0800

    Added PSR-2 compliant curly braces to MediaController

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -1,11 +1,13 @@
 <?php
 namespace App\Http\Controllers;
 
-class MediaController extends BaseController {
+class MediaController extends BaseController
+{
 	/**
 	 * @return \Illuminate\View\View
 	 */
-	public function getIndex() {
+	public function getIndex()
+	{
 		$this->nav('navbar.social.social');
 		$this->title(trans('media.title'));
 		return $this->view('media');
