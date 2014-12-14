f47aa1cfc9243bc8a5d19e756aa666b3ae4a4683
Runis <stntlr@gmail.com>
2014-12-14 13:42:11 -0800

    Added PSR-2 compliant curly braces to App\RuneTime\Forum\Poll

--- app/RuneTime/Forum/Polls/PollRepository.php
+++ app/RuneTime/Forum/Polls/PollRepository.php
@@ -1,11 +1,13 @@
 <?php
 namespace App\RuneTime\Forum\Polls;
 use App\Runis\Core\EloquentRepository;
-class PollRepository extends EloquentRepository {
+class PollRepository extends EloquentRepository
+{
 	/**
 	 * @param Poll $model
 	 */
-	public function __construct(Poll $model) {
+	public function __construct(Poll $model)
+	{
 		$this->model = $model;
 	}
 }
