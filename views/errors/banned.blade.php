b8b26e8db848f1ac49e964f750de32c58bc41baf
Runis <stntlr@gmail.com>
2014-12-13 21:25:19 -0800

    Fixed typo when user banned

--- views/errors/banned.blade.php
+++ views/errors/banned.blade.php
@@ -2,7 +2,7 @@
 @section('contents')
 			<div class='wrapper'>
 				<p class='lead text-danger'>
-					You've Banned!
+					You're Banned!
 				</p>
 				<p class='text-info'>
 					We're sorry, but it looks like you've been banned until {{ $time }}.  This may have been an accident.  If you feel it was an accident please message us over at <a href='https://twitter.com/Rune_Time' title='@lang('footer.twitter')'>Twitter</a>.  Here is the reason that was given for you being banned:
