72ad8ae5bc9c1499dccbe6b3d52f2288b53d396a
Runis <stntlr@gmail.com>
2014-12-14 12:19:22 -0800

    Added proper spacing in BaseController

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -146,6 +146,7 @@ class BaseController extends Controller
 				unset($activity[$key]);
 			}
 		}
+		
 		$activity[microtime(true) * 1000] = $current;
 		$most = \Cache::get('activity.most');
 		$activeCount = count($activity);
