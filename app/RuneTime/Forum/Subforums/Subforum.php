c10404a7b6e156f26dd88f8548f52c1608bf99b1
Runis <stntlr@gmail.com>
2014-12-14 13:42:40 -0800

    Added PSR-2 compliant curly braces to App\RuneTime\Forum\Subforums

--- app/RuneTime/Forum/Subforums/Subforum.php
+++ app/RuneTime/Forum/Subforums/Subforum.php
@@ -5,7 +5,8 @@ use App\RuneTime\Forum\Threads\ThreadRepository;
 use App\Runis\Core\Entity;
 use App\RuneTime\Forum\Threads\PostRepository;
 use App\RuneTime\Forum\Threads\Post;
-class Subforum extends Entity{
+class Subforum extends Entity
+{
 	protected $table = 'forum_subforums';
 	protected $with = [];
 	protected $fillable = ['name', 'description', 'roles', 'posts_enabled', 'posts_active', 'thread_count', 'post_count', 'last_post', 'position', 'parent'];
@@ -18,43 +19,51 @@ class Subforum extends Entity{
 	/**
 	 * @return \Illuminate\Database\Eloquent\Relations\HasMany
 	 */
-	public function threads() {
+	public function threads()
+	{
 		return $this->hasMany('App\RuneTime\Forum\Threads\Thread', 'subforum_id');
 	}
 
 	/**
 	 * @return mixed
 	 */
-	public function lastPost() {
-		if(!$this->cachePosts)
+	public function lastPost()
+	{
+		if(!$this->cachePosts) {
 			$this->cachePosts = with(new PostRepository(new Post))->getByid($this->last_post);
+		}
 		return $this->cachePosts;
 	}
 
 	/**
 	 * @return bool|mixed
 	 */
-	public function lastThread() {
+	public function lastThread()
+	{
 		$post = $this->lastPost();
-		if(!$this->cacheThreads)
+		if(!$this->cacheThreads) {
 			$this->cacheThreads = with(new ThreadRepository(new Thread()))->getById($post->thread[0]->id);
+		}
 		return $this->cacheThreads;
 	}
 
 	/**
 	 * @return bool
 	 */
-	public function isRead() {
+	public function isRead()
+	{
 		if(\Auth::check()) {
 			$lastRead = \Cache::get('user' . \Auth::user()->id . '.subforum#' . $this->id . '.read');
 			$lastPost = $this->lastPost();
-			if(!empty($lastPost))
-				if($lastRead > \Time::getEpoch($this->lastPost()->created_at))
+			if(!empty($lastPost)) {
+				if($lastRead > \Time::getEpoch($this->lastPost()->created_at)) {
 					return true;
-				else
+				} else {
 					return false;
-			elseif(empty($lastPost))
+				}
+			} elseif(empty($lastPost)) {
 				return true;
+			}
 		}
 		return true;
 	}
@@ -62,16 +71,19 @@ class Subforum extends Entity{
 	/**
 	 * @return string
 	 */
-	public function toSlug() {
+	public function toSlug()
+	{
 		return '/forums/' . \String::slugEncode($this->id, $this->name);
 	}
 
 	/**
 	 * @return bool
 	 */
-	public function canPost() {
-		if(\Auth::check() && $this->posts_enabled === true)
+	public function canPost()
+	{
+		if(\Auth::check() && $this->posts_enabled === true) {
 			return true;
+		}
 		return false;
 	}
 }
