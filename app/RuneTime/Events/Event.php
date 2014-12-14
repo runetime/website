9b49ebc12875ccc196d45f8613a4b760d12f47dc
Runis <stntlr@gmail.com>
2014-12-14 13:41:58 -0800

    Added PSR-2 compliant curly braces to App\RuneTime\Events

--- app/RuneTime/Events/Event.php
+++ app/RuneTime/Events/Event.php
@@ -1,7 +1,8 @@
 <?php
 namespace App\RuneTime\Event;
 use App\Runis\Core\Entity;
-class Event extends Entity {
+class Event extends Entity
+{
 	protected $table = 'calendar_events';
 	protected $fillable = ['author_id', 'title', 'contents', 'contents_parsed', 'time_of', 'calendar', 'status'];
 	protected $dates = [];
--- app/RuneTime/Events/EventRepository.php
+++ app/RuneTime/Events/EventRepository.php
@@ -1,11 +1,13 @@
 <?php
 namespace App\RuneTime\Event;
 use App\Runis\Core\EloquentRepository;
-class EventRepository extends EloquentRepository {
+class EventRepository extends EloquentRepository
+{
 	/**
 	 * @param Event $model
 	 */
-	public function __construct(Event $model) {
+	public function __construct(Event $model)
+	{
 		$this->model = $model;
 	}
 }
