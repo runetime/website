4a01bbd7de7f0e3516f68bb5192b08f16c7379d6
Runis <stntlr@gmail.com>
2014-12-16 12:42:15 -0800

    Fixed Issue 1

--- assets/typescript/modules/main.ts
+++ assets/typescript/modules/main.ts
@@ -29,4 +29,9 @@ $(function () {
 			}
 		}
 	});
+	$('.navbar .dropdown').hover(function() {
+		$(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
+	}, function() {
+		$(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp()
+	});
 });
