85ab6f81903f7a7e5982f16e8fed367a55292cc9
Runis <stntlr@gmail.com>
2014-12-16 13:01:13 -0800

    Simplified some modals into includes in staff index

--- views/staff/index.blade.php
+++ views/staff/index.blade.php
@@ -4,44 +4,21 @@
 				<h1>
 					@lang('staff.index.title')
 				</h1>
-				<div class='pretty-row'>
-					<div class='col-xs-6'>
-						<div>
-							<h3>
-								Staff Checkup
-							</h3>
-							<p>
-								<a href='/staff/checkup'>
-									Complete your staff checkup once every Sunday!
-								</a>
-							</p>
-						</div>
+				<div class='blocks clearfix'>
+					<div>
+						<a href='/staff/checkup'>
+							Staff Checkup
+						</a>
 					</div>
-					<div class='col-xs-6'>
-						<div>
-							<h3>
-								Report User
-							</h3>
-							<p>
-								Report a user straight to the admins.
-							</p>
-							<button type='button' class='btn btn-primary' data-toggle='modal' data-target='#modal-report-user'>
-								Report User
-							</button>
-						</div>
+					<div>
+						<a data-toggle='modal' data-target='#modal-report-user'>
+							Report User
+						</a>
 					</div>
-					<div class='col-xs-6'>
-						<div>
-							<h3>
-								Mute User
-							</h3>
-							<p>
-								Temporarily mute a user on the chatbox for one hour.
-							</p>
-							<button type='button' class='btn btn-primary' data-toggle='modal' data-target='#modal-mute-user'>
-								Mute User
-							</button>
-						</div>
+					<div>
+						<a data-toggle='modal' data-target='#modal-mute-user'>
+							Mute User
+						</a>
 					</div>
 				</div>
 			</div>
@@ -129,54 +106,8 @@
 					</div>
 				</div>
 			</div>
-			<div class='modal fade' id='modal-results-good' tabindex='-1' role='dialog' aria-labelledby='modal-results-good' area-hidden='true'>
-				<div class='modal-dialog'>
-					<div class='modal-content'>
-						<div class='modal-header'>
-							<button type='button' class='close' data-dismiss='modal'>
-								<span aria-hidden='true'>&times;</span> <span class='sr-only'>Close</span>
-							</button>
-							<h4 class='modal-title text-success' id='modal-results-good'>
-								Request Sent
-							</h4>
-						</div>
-						<div class='modal-body'>
-							<p class='text-success' rt-hook='staff.panel:results.good.message'>
-								Your request has been successfully sent.
-							</p>
-						</div>
-						<div class='modal-footer'>
-							<button type='button' class='btn btn-success' data-dismiss='modal'>
-								Close
-							</button>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class='modal fade' id='modal-results-bad' tabindex='-1' role='dialog' aria-labelledby='modal-results-bad' area-hidden='true'>
-				<div class='modal-dialog'>
-					<div class='modal-content'>
-						<div class='modal-header'>
-							<button type='button' class='close' data-dismiss='modal'>
-								<span aria-hidden='true'>&times;</span> <span class='sr-only'>Close</span>
-							</button>
-							<h4 class='modal-title text-warning' id='modal-results-bad'>
-								Request Sent
-							</h4>
-						</div>
-						<div class='modal-body'>
-							<p class='text-warning' rt-hook='staff.panel:results.bad.message'>
-								There was an error processing your request.  Please try again.
-							</p>
-						</div>
-						<div class='modal-footer'>
-							<button type='button' class='btn btn-warning' data-dismiss='modal'>
-								Close
-							</button>
-						</div>
-					</div>
-				</div>
-			</div>
+			@include('partials.modals.good')
+			@include('partials.modals.bad')
 			<script>
 				staffPanel = new StaffPanel();
 			</script>
