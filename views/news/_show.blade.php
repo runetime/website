2246a18d0ccdf6b6cf676a7558602a5d097581c6
Runis <stntlr@gmail.com>
2014-12-20 10:54:05 -0800

    Updated news

--- views/news/_show.blade.php
+++ views/news/_show.blade.php
@@ -1,18 +1,16 @@
-			<div class='wrapper'>
+			<div class='news'>
 				<h1>
 					{{ $news->title }}
 				</h1>
 				<p class='text-muted'>
 					{{ \Time::shortTime($news->created_at) }} @lang('utilities.by') {!! \Link::name($news->author_id) !!}
 				</p>
-			</div>
-			<div class='wrapper-dark'>
+				<div class='clearfix'>
 @if($news->hasImage())
-                <img src='/img/news/thumbnail/{{ $news->id }}.png' alt='{{ $news->title }}' class='pull-right img-news img-responsive' />
+					<img src='/img/news/thumbnail/{{ $news->id }}.png' alt='{{ $news->title }}' class='pull-right img-news img-responsive' />
 @endif
+				</div>
 				{!! $news->contents_parsed !!}
-			</div>
-			<div class='wrapper'>
 				@lang('tags.tagged_as')
 				<ul class='list-inline inline'>
 @foreach($news->tags as $tag)
@@ -23,3 +21,4 @@
 					</li>
 @endforeach
 				</ul>
+			</div>
