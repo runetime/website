ad6a02b3e4d6e2d998d39049711600291973eab1
Runis <stntlr@gmail.com>
2014-12-22 15:05:06 -0800

    Changed play.index to flat design

--- views/play/index.blade.php
+++ views/play/index.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.default')
 @section('contents')
-			<div class='wrapper'>
+			<div class='wrapper wrapper-flat'>
 				<h1>
 					@lang('play.index.play_runescape')
 				</h1>
-				<div class='row'>
+				<div class='row row-flat'>
 					<div class='col-xs-12 col-sm-6'>
 						<a href='/play/3'>
 							<img src='/img/play/rs3.png' class='img-responsive center-block' />
