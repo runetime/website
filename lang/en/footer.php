6816f1999860e8c27bb82ec1fb84e02a293e9e6e
Runis <stntlr@gmail.com>
2014-12-27 14:05:14 -0800

    Alphabetically sorted lang/footer

--- lang/en/footer.php
+++ lang/en/footer.php
@@ -1,17 +1,17 @@
 <?php
 return [
+	'about_us'   => 'About Us',
+	'affiliated' => 'RuneTime is in no way affiliated to Runescape.',
+	'and'        => 'and',
+	'contact'    => 'Contact Us',
 	'copyright'  => 'Copyright RuneTime',
+	'facebook'   => 'Follow RuneTime on Facebook',
+	'follow_us'  => 'Follow Us',
+	'images'     => 'All Runescape images are property of Jagex Ltd.',
+	'language'   => 'Click here to set your language',
 	'privacy'    => 'privacy',      // Meant to be lowercase
 	'terms'      => 'terms of use', // Meant to be lowercase
-	'contact'    => 'Contact Us',
-	'and'        => 'and',
 	'trademarks' => 'are trademarks of',
-	'images'     => 'All Runescape images are property of Jagex Ltd.',
-	'affiliated' => 'RuneTime is in no way affiliated to Runescape.',
-	'about_us'   => 'About Us',
-	'follow_us'  => 'Follow Us',
-	'facebook'   => 'Follow RuneTime on Facebook',
 	'twitter'    => 'Follow RuneTime on Twitter',
 	'youtube'    => 'Subscribe to RuneTime on YouTube',
-	'language'   => 'Click here to set your language',
 ];
