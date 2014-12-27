cc3a0e332ad126084e53ef327c8874faf6ca6f8a
Runis <stntlr@gmail.com>
2014-12-27 14:03:57 -0800

    Alphabetically sorted lang/donate

--- lang/en/donate.php
+++ lang/en/donate.php
@@ -1,13 +1,13 @@
 <?php
 return [
 	'title'    => 'Donate to RuneTime',
-	'help'     => 'Want to help-out RuneTime?',
-	'pledge'   => "We have pledged to remain Ad-free and keep those annoying (and often inappropriate!) Ads out of here! But of course we still have to pay the bills! So, any donations from our Amazing Members will be very much appreciated. If you want to donate, and have a PayPal account, then please just click the button below:",
 	'donation' => [
-		'item_name' => 'RuneTime Donation',
 		'cn'        => "What's your RuneTime username?",
+		'item_name' => 'RuneTime Donation',
 	],
-	'note'   => "And unlike some websites, every penny we receive will <span class='text-uppercase'>only</span> be used to fund this website. Any spare money will be used to pay for Bond Give-Away's, other prizes, or donated to Charity. The staff and owners of RuneTime will not receive a penny! For us, it’s all about the Community. For reasons of transparency, each month we will post a detailed account of any money received and exactly where it was spent. There will be no secrets here! So, if you can and want to help us out, then please click the \"Donate\" button above.",
-	'thanks' => 'Many Thanks,',
 	'head'   => 'Head of RuneTime',
+	'help'   => 'Want to help-out RuneTime?',
+	'note'   => "And unlike some websites, every penny we receive will <span class='text-uppercase'>only</span> be used to fund this website. Any spare money will be used to pay for Bond Give-Away's, other prizes, or donated to Charity. The staff and owners of RuneTime will not receive a penny! For us, it’s all about the Community. For reasons of transparency, each month we will post a detailed account of any money received and exactly where it was spent. There will be no secrets here! So, if you can and want to help us out, then please click the \"Donate\" button above.",
+	'pledge' => "We have pledged to remain Ad-free and keep those annoying (and often inappropriate!) Ads out of here! But of course we still have to pay the bills! So, any donations from our Amazing Members will be very much appreciated. If you want to donate, and have a PayPal account, then please just click the button below:",
+	'thanks' => 'Many Thanks,',
 ];
