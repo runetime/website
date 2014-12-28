77b943074c8aa7d050f4cf9e02dff37ecceae077
Runis <stntlr@gmail.com>
2014-12-27 16:02:14 -0800

    Updated partials._activity to be PSR-2 compliant

--- views/partials/_activity.php
+++ views/partials/_activity.php
@@ -1,9 +1,15 @@
 <?php
-if(!isset($activity)) $activity = [];
+if(!isset($activity)) {
+	$activity = [];
+}
+
 $list = "";
 $activity = array_reverse($activity);
-foreach($activity as $key => $user)
-	if($user['logged'] === true)
+foreach($activity as $key => $user) {
+	if($user['logged'] === true) {
 		$list .= "<span " . \String::tooltip("Last active " . \Time::shortReadable($user['time'])) . ">" . \Link::name($user['user']) . "</span>, ";
+	}
+}
+
 $list = substr($list, 0, -2);
 echo $list;
