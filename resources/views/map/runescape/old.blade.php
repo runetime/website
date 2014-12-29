a300a84bb5dbd8cefb75d1a56e4904ea06a099e9
Runis <stntlr@gmail.com>
2014-12-28 22:23:22 -0800

    Updated maps to be full-width of wrapper

--- resources/views/map/runescape/old.blade.php
+++ resources/views/map/runescape/old.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.default')
 @section('contents')
-			<div class='wrapper'>
+			<div class='wrapper wrapper-flat'>
 				<script src='http://zoom.it/wVYC.js?width=auto&height=700px'></script>
 			</div>
 @stop
