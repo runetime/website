19781f00d961eb14ed6e829d0b90bf0018cd9937
Runis <stntlr@gmail.com>
2014-12-28 22:02:14 -0800

    Updated forums.tags.view to use included news view

--- resources/views/forums/tags/view.blade.php
+++ resources/views/forums/tags/view.blade.php
@@ -8,27 +8,7 @@
 					@lang('news.title')
 				</h3>
 @foreach($news as $newsPiece)
-                <div>
-                    <h3>
-                        {{ $newsPiece->title }}
-                    </h3>
-                    <span class='text-muted'>{{ Time::long($newsPiece->created_at) }}</span> by {!! \Link::Name($newsPiece->author_id) !!}
-                    <p>
-                        {!! $newsPiece->contents_parsed !!}
-                    </p>
-                    <ul class='list-inline'>
-                        <li>
-                            <a href='{{ $newsPiece->toSlug() }}' title='{{ $newsPiece->title }}'>
-                               @lang('utilities.read_more')
-                            </a>
-                        </li>
-                        <li>
-                            <a href='{{ $newsPiece->toSlug('#comments') }}') }}'>
-                                @lang('utilities.comments', ['amount' => $newsPiece->post_count])
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+	@include('news._show', ['newsPiece' => $newsPiece])
 @endforeach
 				<h3>
 					@lang('forums.thread.titles')
