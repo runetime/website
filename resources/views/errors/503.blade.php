04d744db353642e30604dfef0a585960f9d43624
Runis <stntlr@gmail.com>
2014-12-28 22:23:44 -0800

    Updated maintenance page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,1 +1,11 @@
-Be right back!
+@extends('layouts.default')
+@section('contents')
+			<div class='wrapper'>
+				<h1>
+					Under Maintenance
+				</h1>
+                <p>
+                    We are currently under maintenance.  We'll be back shortly!
+                </p>
+			</div>
+@stop
