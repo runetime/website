038058effba77de8e9cab31f45317c738cb3babd
Runis <stntlr@gmail.com>
2015-01-17 21:33:41 -0800

    Fixed Issue 55

--- resources/views/signatures/style.blade.php
+++ resources/views/signatures/style.blade.php
@@ -1,14 +1,21 @@
 @extends('layouts.default')
 @section('contents')
 			<div class='wrapper'>
-				<div class='row'>
+<?php $x = 1; ?>
 @foreach($imgs as $img)
+	@if($x === 1)
+				<div class='row'>
+	@endif
 					<div class='col-xs-12 col-sm-6 col-md-3'>
 						<a href='/signatures/username={{ $username }}/type={{ $type }}/style={{ str_replace('.png', '', $img) }}'>
 							<img src='/img/signatures/backgrounds/{{ $img }}' />
 						</a>
 					</div>
-@endforeach
+<?php $x += 1; ?>
+	@if($x === 5)
 				</div>
+<?php $x = 1; ?>
+	@endif
+@endforeach
 			</div>
 @stop
