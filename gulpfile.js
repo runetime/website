f6fc1108ca97a743ec88e0c9edd6ed6de37e8da8
Austin Hellyer <hello@austinhellyer.me>
2015-05-26 19:42:41 -0700

    Fix the Gulpfile and remove TypeScript compilation
    This fixes the Gulpfile, so that it no longer errors. This also simplifies the
    file.
    
    This now supports SCSS compilation and concat of the JavaScript vendor files.
    
    I am thinking of removing the TypeScript and converting it to plain JavaScript,
    but that's a change for another Pull Request. :)
    
    Signed-off-by: Austin Hellyer <hello@austinhellyer.me>

--- gulpfile.js
+++ gulpfile.js
@@ -6,86 +6,32 @@ var gulp = require('gulp');
 /**
  * Gulp dependencies.
  */
-var concat = require('gulp-concat');
-var jshint = require('gulp-jshint');
-var uglifyjs = require('gulp-uglifyjs');
-var rename = require('gulp-rename');
-var sass = require('gulp-ruby-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var ts = require('gulp-typescript');
-
-/**
- * Abstracted TypeScript settings
- *
- * @type {{declarationFiles: boolean, noExternalResolve: boolean, sortOutput: boolean}}
- */
-var tsProject = {
-	declarationFiles: true,
-	noExternalResolve: true,
-	sortOutput: true
-};
+var concat = require('gulp-concat'),
+	sass = require('gulp-sass');
 
 /**
  * Directories of the various assets and public output.
  *
- * @type {{assets: {scss: string, typescript: string}, public: {css: string, js: string}}}
+ * @type object
  */
 var paths = {
-	'assets': {
-		scss: './resources/assets/scss/*.scss',
-		typescript: './resources/assets/typescript/modules/*.ts'
+	assets: {
+		scss: './resources/assets/scss/**/*.scss',
+		javascript_vendor: './resources/assets/typescript/vendor/*.js'
 	},
-	'public': {
+	public: {
 		css: './public/css',
 		js: './public/js'
 	}
 };
 
 /**
- * Sass compile task.
+ * Sass Compilation task.
  */
-gulp.task('scss', function() {
-	return gulp.src(paths.assets.scss)
-		.pipe(sass({
-			sourcemap: false,
-			style: 'compressed'
-		}))
-		.on('error', function(err) {
-			console.log(err.message);
-		})
-		.pipe(gulp.dest(paths.public.css));
-});
-
-/**
- * Admin TypeScript compile task.
- */
-gulp.task('scripts-admin', function() {
-	var tsResult = gulp.src('./resources/assets/typescript/admin/*.ts')
-		.pipe(sourcemaps.init())
-		.pipe(ts({
-			sortOutput: true
-		}));
-
-	return tsResult.js
-		.pipe(uglifyjs('admin.js'), {
-			compress: true
-		})
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('./public/js/'));
-});
-
-/**
- * Modules TypeScript compile task.
- */
-gulp.task('scripts-modules', function() {
-	var tsResult = gulp.src('./resources/assets/typescript/modules/*.ts')
-		.pipe(sourcemaps.init())
-		.pipe(ts(tsProject));
-
-	return tsResult.js
-		.pipe(concat('modules.js'))
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('./public/js'));
+gulp.task('sass', function () {
+  gulp.src(paths.assets.scss)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(paths.public.css));
 });
 
 /**
@@ -103,12 +49,10 @@ gulp.task('scripts-vendor', function() {
 		'./resources/assets/typescript/vendor/dragslideshow.js',
 		'./resources/assets/typescript/vendor/jasny-bootstrap.js'
 	];
-	gulp.src(src)
-		.pipe(uglifyjs('vendor.js', {
-			compress: true,
-			outSourceMap: false
-		}))
-		.pipe(gulp.dest('./public/js'))
+
+	return gulp.src(paths.assets.javascript_vendor)
+		.pipe(concat('vendor.js'))
+		.pipe(gulp.dest(paths.public.js));
 });
 
 /**
@@ -116,16 +60,11 @@ gulp.task('scripts-vendor', function() {
  * changes and runs the appropriate Gulp task.
  */
 gulp.task('watch', function() {
-	gulp.watch('./resources/assets/typescript/*', ['scripts-admin', 'scripts-modules', 'scripts-vendor']);
-	gulp.watch('./resources/assets/typescript/admin/*.ts', ['scripts-admin']);
-	gulp.watch('./resources/assets/typescript/modules/*.ts', ['scripts-modules']);
-	gulp.watch('./resources/assets/typescript/vendor/*.js', ['scripts-vendor']);
-	gulp.watch('./resources/assets/scss/*.scss', ['scss']);
-	gulp.watch('./resources/assets/scss/partials/*.scss', ['scss']);
-	gulp.watch('./resources/assets/scss/partials/*/*.scss', ['scss']);
+	gulp.watch(paths.assets.javascript_vendor, ['scripts-vendor']);
+	gulp.watch(paths.assets.scss, ['scss']);
 });
 
 /**
  * Runs all of the compilations on the default Gulp command.
  */
-gulp.task('default', ['scss', 'scripts-admin', 'scripts-modules', 'scripts-vendor']);
+gulp.task('default', ['sass', 'scripts-vendor']);
