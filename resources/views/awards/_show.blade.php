0133e79ee662aa4536d51bc931032053d7a5c8a8
Austin Hellyer <taiyaei@users.noreply.github.com>
2015-06-14 10:16:57 -0700

    CS
    Whoops.

--- resources/views/awards/_show.blade.php
+++ resources/views/awards/_show.blade.php
@@ -1,6 +1,6 @@
 <div class='card clearfix'>
     <div class='pull-left'>
-        <img src='/img/awards/{{ $award->id }}.png' alt='{{ $award->name }}' class='img-responsive' title='{{ $award->name}}' />
+        <img src='/img/awards/{{ $award->id }}.png' alt='{{ $award->name }}' class='img-responsive' title='{{ $award->name }}' />
 @if(isset($at))
         <p>
             @lang('awards.awarded_on', ['date' => \Time::DMY($at)])
